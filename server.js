a38b54d3448cb570adc919ee4047eb6b14cea5bb
Napordisch <bogdankhamidullin@gmail.com>
2024-10-21 23:34:59 +0500

    Normal path

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express')
+path = require('path')
 const app = express()
 const port = 3000
 
 app.use(express.static("login-page"));
 
 app.get('/', (req, res) => {
-  res.sendFile("login.html");
+  res.sendFile("login-page/login.html", { root: __dirname });
 })
 
 app.listen(port, () => {
