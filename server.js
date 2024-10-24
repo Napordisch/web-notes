62809a580b6091f8277b3811ce5cfda8d1d23cec
Napordisch <bogdankhamidullin@gmail.com>
2024-10-25 00:55:35 +0500

    notes page

--- server.js
+++ server.js
@@ -4,9 +4,12 @@ const app = express()
 const port = 3000
 
 app.use(express.static("login-page"));
+app.use(express.static("note-page"));
+app.use(express.static("common-static"));
 
 app.get('/', (req, res) => {
-  res.sendFile("login-page/login.html", { root: __dirname });
+  // res.sendFile("login-page/login.html", { root: __dirname });
+  res.sendFile("note-page/note.html", { root: __dirname });
 })
 
 app.listen(port, () => {
