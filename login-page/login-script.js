378d0edb6492b54dc95de1d6245aeaffe470338a
Napordisch <bogdankhamidullin@gmail.com>
2024-10-28 23:21:52 +0500

    register error path

--- login-page/login-script.js
+++ login-page/login-script.js
@@ -15,52 +15,57 @@ function ClearProblem() {
     ShowProblem("");
 }
 
-/*
+
 function Register() {
     let fields = GetFields();
     if (fields === false) {
-        /!*document.getElementById("form-message").innerText = "Напишите электронную почту и пароль.";*!/
         ShowProblem("Напишите электронную почту и пароль.")
         return;
     }
-    if (users[fields.email] !== undefined) {
-
-        ShowProblem("Эта почта уже использована для создания аккаунта. Если вы знаете пароль, можете войти.");
-        return;
-    }
-    if (users[fields.email] === undefined) {
-        users[fields.email] = fields.password;
-    }
-    console.log(users);
-    WriteToUserDB(users);
+    fetch("/register", {
+        method: "POST", headers: {
+            "Content-Type": "Application/json",
+        }, body: JSON.stringify(fields)
+    }).then(res => {
+        if (res.status === 409) {
+            return res.text().then(text => {
+                console.error(text);
+                throw new Error(text);
+            })
+        }
+    }).catch(error => {
+        if (error.message === "email-already-exists") {
+            ShowProblem("Аккаунт на такую почту уже зарегистрирован");
+        }
+    })
 }
-*/
 
 function Login() {
     let fields = GetFields();
     if (fields === false) {
         ShowProblem("Напишите электронную почту и пароль.");
+        return;
     }
-    else {
-        console.log(JSON.stringify(fields));
-        fetch("/login", {
-            method: "POST",
-            headers: {
-                "Content-Type": "Application/json",
-            },
-            body: JSON.stringify(fields)
-        }).then(res => {
-            if (res.status === 401) {
-                throw new Error("no-such-user");
-            }
-            localStorage.setItem("email", fields.email);
-            localStorage.setItem("password", fields.password);
-            window.location.href = "/all-notes";
-        }).catch(error => {
-            console.log(error);
-            if (error.message === "no-such-user") {
-                ShowProblem("Неправильный логин или пароль");
-            }
-        })
-    }
+    console.log(JSON.stringify(fields));
+    fetch("/login", {
+        method: "POST", headers: {
+            "Content-Type": "Application/json",
+        }, body: JSON.stringify(fields)
+    }).then(res => {
+        if (res.status === 401) {
+            return res.text().then(text => {
+                console.error(text);
+                throw new Error(text)
+            });
+        }
+        localStorage.setItem("email", fields.email);
+        localStorage.setItem("password", fields.password);
+        window.location.href = "/all-notes";
+    }).catch(error => {
+        console.log(error);
+        if (error.message === "no-such-user") {
+            ShowProblem("Неправильный логин или пароль");
+        }
+    })
+
 }
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 path = require('path')
+const fs = require("fs");
 const app = express()
 const port = 3000;
 
@@ -10,41 +11,59 @@ app.use(express.static("note-page"));
 app.use(express.static("common-static"));
 app.use(express.static("notes-viewer-page"));
 
-users = {a: {
-  password:  "s",
-    id: "1"
-}
-}
+// users = JSON.parse(fs.readFileSync('test-users.json', 'utf8'));
+// console.log("Users:")
+// console.log(users);
 
+function getUsers() {
+    return JSON.parse(fs.readFileSync('test-users.json', 'utf8'));
+}
+console.log(getUsers())
 
 app.get('/login', (req, res) => {
-  res.sendFile("login-page/login.html", { root: __dirname });
+    res.sendFile("login-page/login.html", {root: __dirname});
 })
 
 app.get('/all-notes', (req, res) => {
-  res.sendFile("notes-viewer-page/all-notes.html", { root: __dirname });
+    res.sendFile("notes-viewer-page/all-notes.html", {root: __dirname});
 })
 
 app.get('/note', (req, res) => {
-  res.sendFile("note-page/note.html", { root: __dirname });
+    res.sendFile("note-page/note.html", {root: __dirname});
 })
 
 app.get('/', (req, res) => {
-  res.redirect(req.baseUrl + '/login');
+    res.redirect(req.baseUrl + '/login');
 })
 
 app.post('/login', (req, res) => {
-  console.log(req.body)
-  let fields = req.body;
-  if (fields.email in users && fields.password === users[fields.email].password) {
-    res.status(400);
-    res.send();
-    return;
-  }
-  res.status(401).json({message: "Login failed"});
-  res.send();
+    let users = getUsers();
+    console.log(users);
+    console.log(req.body);
+    let fields = req.body;
+    if (fields.email in users && fields.password === users[fields.email].password) {
+        res.status(400);
+        res.send();
+        return;
+    }
+    let errorMessage = "no-such-user";
+    res.status(401).send(errorMessage);
+    cons
+})
+
+app.post("/register", (req, res) => {
+    let users = getUsers();
+    console.log(users);
+    console.log (req.body);
+    let fields = req.body;
+    if (fields.email in users) {
+        let errorMessage = "email-already-exists";
+        console.error(errorMessage);
+        res.status(409).send(errorMessage);
+        return;
+    }
 })
 
 app.listen(port, () => {
-  console.log(`Web-notes app listening on port ${port}`)
+    console.log(`Web-notes app listening on port ${port}`)
 })
